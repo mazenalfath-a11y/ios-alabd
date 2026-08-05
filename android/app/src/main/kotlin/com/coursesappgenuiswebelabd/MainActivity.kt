16ad8abd96368d86d13ac5b24e9e530f297dc2d7
Your Name <your_email@gmail.com>
2026-08-05 18:38:21 +0300

    Add missing iOS and Android platform folders without large dump files

---
+++ android/app/src/main/kotlin/com/coursesappgenuiswebelabd/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.coursesappgenuiswebelabd
+
+import android.os.Bundle
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 🚫 Prevent screenshots and screen recording
+        window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
+    }
+}
